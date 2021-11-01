e52d80b00ccd06d3561b5865ca39c3c1ffdc24a4
janfari <14154218+janfari@users.noreply.github.com>
2021-10-31 20:26:38 -0400

    Factor out bus pointer

--- src/dbusServer.c
+++ src/dbusServer.c
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 #include "dbusServer.h"
+#include "dbus.h"
 
 
 /**
--- src/main.c
+++ src/main.c
@@ -1,3 +1,8 @@
+/******************************************************************************
+* @file    main.c
+* @brief
+******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,13 +14,13 @@
 /* This name is permitted in /etc/dbus-1/system.d/com.example.DbusServiceC.conf
  * To change this service name, the .conf will need to be edited accordingly */
 const char dbusServiceName[] = "com.example.DbusServiceC";
-sd_bus * bus = NULL;
+sd_bus * pBus = NULL;
 
 
 /**
  * @brief      Main loop to start and maintain the dbus framework
- * @details    The main function forms a connection to the user bus, 
- *             installs dbus objects, then enters an infinite loop to 
+ * @details    The main function forms a connection to the user bus,
+ *             installs dbus objects, then enters an infinite loop to
  *             process new dbus calls.
  * @return     sdbusErr sd_bus error code
  **/
@@ -24,25 +29,26 @@ int main()
     int sdbusErr = 0;
 
     /* Connect to the user (session) bus. */
-    sdbusErr = sd_bus_default_user(&bus);
+    sdbusErr = sd_bus_default_user(&pBus);
     /* This would connect you to the system bus instead
-    sdbusErr = sd_bus_default_system(&bus); */
+    sdbusErr = sd_bus_default_system(&pBus); */
 
     /* sd_bus calls return negative values to indicate errors */
     if (sdbusErr < 0)
     {
-        /* If you want to know more about the possible errors, something like this should 
-        give you a sense of what exists https://fossies.org/dox/glibc-2.34/errlist_8h_source.html */
+        /* If you want to know more about the possible errors,
+        something like this should give you a sense of what exists
+        https://fossies.org/dox/glibc-2.34/errlist_8h_source.html */
         printf("D-Bus Error: Failed to connect to user bus:%s\n", strerror(-sdbusErr));
-        
+
         /* Clean up by releasing the bus */
-        sd_bus_unref(bus);
+        sd_bus_unref(pBus);
         return sdbusErr;
     }
 
     /* Install an object A (We will use A as a server) */
     sdbusErr = sd_bus_add_object_vtable(
-        bus,
+        pBus,
         NULL,
         objA_objectPath,
         objA_interfaceName,
@@ -52,24 +58,24 @@ int main()
     if (0 > sdbusErr)
     {
         printf("D-Bus Error: Failed to add object vtable: %s\n", strerror(-sdbusErr));
-        sd_bus_unref(bus);
+        sd_bus_unref(pBus);
         return sdbusErr;
     }
 
     /* Take a pre-defined unique service name */
-    sdbusErr = sd_bus_request_name(bus, dbusServiceName, 0);
+    sdbusErr = sd_bus_request_name(pBus, dbusServiceName, 0);
 
     if (0 > sdbusErr)
     {
         printf("D-Bus Error: Failed to request service name: %s\n", strerror(-sdbusErr));
-        sd_bus_unref(bus);
+        sd_bus_unref(pBus);
         return sdbusErr;
     }
 
     for (;;)
     {
         /* Process D-Bus requests */
-        sdbusErr = sd_bus_process(bus, NULL);
+        sdbusErr = sd_bus_process(pBus, NULL);
 
         if (0 > sdbusErr)
         {
@@ -79,16 +85,15 @@ int main()
         }
 
         /* Wait (in usecs) for the next request to be ready */
-        sdbusErr = sd_bus_wait(bus, 100);
+        sdbusErr = sd_bus_wait(pBus, 100);
 
         if (0 > sdbusErr)
         {
             printf("D-Bus Error: Failed to wait for bus: %s\n", strerror(-sdbusErr));
         }
-
     }
 
     /* Clean up */
-    sd_bus_unref(bus);
+    sd_bus_unref(pBus);
     return sdbusErr;
 }
